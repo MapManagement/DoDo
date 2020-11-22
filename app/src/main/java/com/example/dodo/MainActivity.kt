4066ec81394782393d1ef28b6a2ea2385f126b36
Jan <fortnitetest@mail.de>
2020-11-22 15:51:21 +0100

    configured pre settings

--- app/src/main/java/com/example/dodo/MainActivity.kt
+++ app/src/main/java/com/example/dodo/MainActivity.kt
@@ -1,38 +1,28 @@
 package com.example.dodo
 
+import android.app.AlertDialog
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
+        fab_add_entry.setOnClickListener { addNewToDoitem() }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+    private fun addNewToDoitem() {
+        val alert = AlertDialog.Builder(this)
+        val itemText = EditText(this)
+        alert.setTitle("ToDo Item")
+        alert.setView(itemText)
+        alert.setPositiveButton("Add") { _, _ ->
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
         }
+        alert.show()
     }
 }
