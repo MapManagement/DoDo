98c7f14ed532225751b7287cf0c3743c91f5da61
Jan <fortnitetest@mail.de>
2020-11-23 22:53:47 +0100

    added custom element for entry dialogs

---
+++ app/src/main/java/com/example/dodo/CustomEntryDialog.kt
@@ -0,0 +1,11 @@
+package com.example.dodo
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+class CustomEntryDialog: DialogFragment() {
+
+}
