5670e868b3ce947b0483d971acc92562b16d1885
Jan <fortnitetest@mail.de>
2020-12-13 17:28:46 +0100

    replaced custom entry class

--- app/src/main/java/com/example/dodo/CustomEntryDialog.kt
+++ app/src/main/java/com/example/dodo/CustomEntryDialog.kt
@@ -3,10 +3,19 @@ package com.example.dodo
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.custom_entry_dialog.*
 
 class CustomEntryDialog(context: Context): Dialog(context) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.custom_entry_dialog)
+
+        // entry_submit_button.setOnClickListener { getEntryText()}
     }
+
+    public fun getEntryText(): String {
+        return entry_text.text.toString()
+    }
+
 }
--- app/src/main/java/com/example/dodo/MainActivity.kt
+++ app/src/main/java/com/example/dodo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.dodo
 import android.app.AlertDialog
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,10 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab_add_entry.setOnClickListener { addNewToDoitem() }
+        fab_add_entry.setOnClickListener { showAlertDialog() }
+    }
+
+    private fun showAlertDialog() {
+        val editText = EditText(this)
+        val dialogView = layoutInflater.inflate(R.layout.custom_entry_dialog, null)
+        AlertDialog.Builder(this)
+            .setTitle("Add New Task")
+            .setView(dialogView)
+            .setPositiveButton("Add") { dialog, which ->
+                addNewToDoitem()
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                Toast.makeText(this, "Cancelled!", Toast.LENGTH_SHORT).show()
+            }
+            .show()
     }
 
     private fun addNewToDoitem() {
-        CustomEntryDialog(this).show()
+
     }
 }
