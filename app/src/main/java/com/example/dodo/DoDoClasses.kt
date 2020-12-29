8aee13f1528714e05fcbf5d2ce454fe4bcf7eb5f
Jan <jannaj02@protonmail.com>
2020-12-29 03:03:25 +0100

    updated DatabaseConnector class, added DoDoClasses.kt

---
+++ app/src/main/java/com/example/dodo/DoDoClasses.kt
@@ -0,0 +1,17 @@
+package com.example.dodo
+
+class ToDoTask {
+    var taskID: Int = 0
+    var taskText: String = ""
+    var isDone: Boolean = false
+    var isDeleted: Boolean = false
+}
+
+class Note {
+    var noteID: Int = 0
+    var noteText: String = ""
+    var isVisible: Boolean = true
+    var isHighlighted: Boolean = false
+    var noteColor: String = "#84A5B9"
+    var isDeleted: Boolean = false
+}
--- app/src/main/java/com/example/dodo/ToDoTask.kt
+++
@@ -1,8 +0,0 @@
-package com.example.dodo
-
-class ToDoTask {
-    var taskID: Int = 0
-    var taskText: String = ""
-    var isDone: Boolean = false
-    var isDeleted: Boolean = false
-}
