091ff6c10c1aaf86d7aa8bb4bf852c272536f24c
Jan <jannaj02@protonmail.com>
2021-04-17 23:21:49 +0200

    worked on set_data fragment for notes

--- app/src/main/java/com/example/dodo/fragments/NoteSetDataFragment.kt
+++ app/src/main/java/com/example/dodo/fragments/NoteSetDataFragment.kt
@@ -1,34 +1,29 @@
 package com.example.dodo.fragments
 
+import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
+import com.example.dodo.DatabaseConnector
 import com.example.dodo.R
+import kotlinx.android.synthetic.main.custom_entry_dialog.*
+import kotlinx.android.synthetic.main.custom_entry_dialog.blue_seekbar
+import kotlinx.android.synthetic.main.custom_entry_dialog.color_preview_button
+import kotlinx.android.synthetic.main.custom_entry_dialog.green_seekbar
+import kotlinx.android.synthetic.main.custom_entry_dialog.red_seekbar
+import kotlinx.android.synthetic.main.fragment_note_set_data.*
+import kotlinx.android.synthetic.main.fragment_todo_set_data.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NoteSetDataFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class NoteSetDataFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var dbConnector: DatabaseConnector
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +33,18 @@ class NoteSetDataFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_note_set_data, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteSetDataFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteSetDataFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //ToDo: adding view stuff
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        this.dbConnector = DatabaseConnector(context, null)
+    }
+
+    private fun insertNewNote() {
+        dbConnector.insertNewNote(note_data_text.text.toString(), note_title_text.text.toString(), "")
+        //ToDo: adding color
     }
 }
