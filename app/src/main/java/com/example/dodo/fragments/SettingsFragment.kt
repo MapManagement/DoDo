6efa0e6b64aac40344eb977083cfa5352435c8fa
Jan <jannaj02@protonmail.com>
2021-06-16 21:57:58 +0200

    first implementation of SettingsFragment.kt

--- app/src/main/java/com/example/dodo/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/dodo/fragments/SettingsFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.preference.PreferenceFragmentCompat
 import com.example.dodo.DatabaseConnector
 import com.example.dodo.MainActivity
 import com.example.dodo.R
@@ -14,18 +15,14 @@ import com.example.dodo.R
  * Use the [SettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+class SettingsFragment : PreferenceFragmentCompat() {
 
     private lateinit var dbConnector: DatabaseConnector
     private lateinit var serverConnector: String
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         dbConnector =  (activity as MainActivity).dbConnector
         serverConnector = (activity as MainActivity).serverConnector
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        setPreferencesFromResource(R.xml.fragment_settings, rootKey)
     }
 }
